b5338fe1df52c824fb5ecef0cb91f28bb0035ae5
leesangwook <8go4go@gmail.com>
2024-11-15 14:25:01 +0900

    Initial commit

---
+++ src/main/java/org/k8go4go/t2t/T2tApplication.java
@@ -0,0 +1,13 @@
+package org.k8go4go.t2t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2tApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2tApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/k8go4go/t2t/T2tApplicationTests.java
@@ -0,0 +1,13 @@
+package org.k8go4go.t2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2tApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
